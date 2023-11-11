bf27f98e989268c72acb1ec76e13aded89bbf2a7
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-11 21:14:06 +0100

    insert into mongo with python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Insert into a db based on kwargs'''
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Inserts into the db'''
+    document = mongo_collection.insert(kwargs)
+
+    return document.inserted_id
