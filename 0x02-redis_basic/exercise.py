6c59c8458d2d1000d9fba79e8b1d474523a99b6d
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-15 04:28:13 +0100

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,19 @@
 
 import redis
 import uuid
+from functools import wraps
 from typing import Callable, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    '''Tracks the number of times the class is called'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -13,6 +25,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Generates random key and stores data in redis using key'''
         key = str(uuid.uuid4())
