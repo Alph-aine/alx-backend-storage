bee927d58be479f13c1925695aaae7dc34779dbd
Abdulbasit <aabdulrafiu721@gmail.com>
2023-11-15 10:15:03 +0100

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,23 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    '''stores history of inputs and outputs of a function'''
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{key}:inputs"
+        output_key = f"{key}:outputs"
+
+        self._redis.rpush(input_key, str(args))
+        data = method(self, *args, **kwargs)
+
+        self._redis.rpush(output_key, str(data))
+        return data
+    return wrapper
+
+
 class Cache:
     '''cache class'''
     def __init__(self):
@@ -26,6 +43,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Generates random key and stores data in redis using key'''
         key = str(uuid.uuid4())
